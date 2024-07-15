23764a2026c10ea3d034aff18f90636606a2a62b
Leonardo Reyes <reyesleonardo2006@gmail.com>
2024-07-15 02:19:32 -0400

    Cambie el manejo de los puertos

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const app = require('./app');
 const http = require('http');
 
-const server = http.createServer(app)
+// Usa el puerto especificado por Render a través de la variable de entorno `PORT`,
+// o usa el puerto 3000 para desarrollo local
+const PORT = process.env.PORT || 3000;
 
-server.listen(3003,() => {
-    console.log('El servidor esta corriendo');
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`El servidor está corriendo en el puerto ${PORT}`);
 });
